db6c8dc9509ad3f7473ea5a5d1e1b367b88bd652
RomanVlah7 <134915065+RomanVlah7@users.noreply.github.com>
2025-02-23 16:50:00 +0200

    Delete NotificationService directory

--- NotificationService/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NotificationService.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
